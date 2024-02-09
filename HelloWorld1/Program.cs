69e70587079c2b2e9e624257d0f1573cc6890388
DELL <DELL@DESKTOP-QPDSD2Q>
2024-02-09 10:18:14 +0100

    Add project files.

---
+++ HelloWorld1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace HelloWorld1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string imie = "Filip";
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Nazywam sie Filip Kazimierczak");
+        }
+    }
+}
+
+
